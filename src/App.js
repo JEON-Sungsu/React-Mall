513041a2dbc4824359abf4c76667faa45623e64a
전성수 <anagma@sjsofttech.co.kr>
2022-11-29 10:29:07 +0900

    컴포넌트 데이터 바인딩

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container, Nav  } from 'react-bootstrap';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import products from './images/mainBg.png';
+import {useState} from "react";
+import data from './data'
 
 function App() {
-  return (
-    <>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+    let [shoes, shoesSet] = useState(data);
 
-        <div className={"main-bg"}></div>
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">리액트 샵</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">메인</Nav.Link>
+                        <Nav.Link href="#features">장바구니</Nav.Link>
+                        <Nav.Link href="#pricing">정보</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
 
-        <div className="container">
-            <div className="row">
-                <div className={"col-lg-4"}>
-                    <img src="" alt=""/>
-                    <h4></h4>
-                    <p></p>
+            <div className={"main-bg"}></div>
+
+            <div className="container">
+                <div className="row">
+                    {
+                        shoes.map(function(item,index){
+                            return(
+                                <ItemList data = {shoes} index = {index} key={index}/>
+                            )
+                        })
+                    }
                 </div>
-                <div className={"col-lg-4"}></div>
-                <div className={"col-lg-4"}></div>
             </div>
+        </>
+    );
+}
+
+function ItemList(props) {
+    return (
+
+        <div className={"col-lg-4"}>
+            <img src={'https://codingapple1.github.io/shop/shoes'+ (props.index+1) +'.jpg'} style={{width: '80%', height: '80%'}}/>
+            <h4>{props.data[props.index].title}</h4>
+            <p>{props.data[props.index].price}</p>
         </div>
-    </>
-  );
+    )
 }
 
 export default App;
