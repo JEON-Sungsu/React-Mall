b1ccacafc86cf0cd6c613a9b54e71b314a4c6619
전성수 <anagma@sjsofttech.co.kr>
2022-12-02 11:16:55 +0900

    프리티어,린트 설정 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,19 @@
-import { useEffect } from "react";
-import {useParams} from "react-router-dom";
-import {useState} from "react";
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
+function Detail(props) {
+    useEffect(() => {
+        console.log('hi');
+    });
 
-function Detail(props){
-
-    useEffect(()=> {
-        console.log('hi')
-    })
-
-    let {id} = useParams()
+    let { id } = useParams();
     return (
         <>
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <img src={'https://codingapple1.github.io/shop/shoes'+ (Number(id) + 1) +'.jpg'} width="100%" />
+                        <img src={'https://codingapple1.github.io/shop/shoes' + (Number(id) + 1) + '.jpg'} width="100%" />
                     </div>
                     <div className="col-md-6">
                         <h4 className="pt-5">{props.data[id].title}</h4>
@@ -26,7 +24,7 @@ function Detail(props){
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Detail;
